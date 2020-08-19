a1c54956b0eec0bd53aae7de204412dcce4bc238
Leo Goodstadt <github@llew.org.uk>
2020-08-19 02:51:52 +0100

    Add examples for docs

---
+++ examples/simple_exp.cpp
@@ -0,0 +1,44 @@
+    #include <interval_dict/icl_interval_map_adaptor.h>
+    #include <interval_dict/intervaldict.h>
+    #include <interval_dict/gregorian.h>
+
+    #include <iostream>
+
+int main ()
+{
+    using namespace interval_dict;
+    using namespace interval_dict::date_literals;
+    using namespace std::string_literals;
+
+    //! [Simple Example]
+    // Only allow closed intervals
+    using Interval = boost::icl::closed_interval<boost::gregorian::date>;
+    using DateDict = IntervalDictExp<std::string, int,
+                                        Interval,
+                                        implementation::IntervalDictICLSubMap<int, Interval>>;
+
+    DateDict interval_dict(DateDict::KeyValueIntervals
+    {
+        {"aa"s, 0, {20200110_dt, 20200120_dt}},
+        {"aa"s, 1, {20200115_dt, 20200125_dt}},
+        {"bb"s, 2, {20200201_dt, 20200205_dt}},
+        {"bb"s, 3, {20200228_dt, 20200228_dt}},
+    });
+
+    std::cout << interval_dict << "\n";
+
+    //! [Simple Example]
+
+    /*
+       //! [Output]
+        aa	[0]	[2020-Jan-10,2020-Jan-14]
+        aa	[0, 1]	[2020-Jan-15,2020-Jan-20]
+        aa	[1]	[2020-Jan-21,2020-Jan-25]
+        bb	[2]	[2020-Feb-01,2020-Feb-05]
+        bb	[3]	[2020-Feb-28,2020-Feb-28]
+       //! [Output]
+    */
+
+
+    return 0;
+}
---
+++ examples/simple_icl.cpp
@@ -0,0 +1,39 @@
+    #include <interval_dict/intervaldicticl.h>
+    #include <interval_dict/gregorian.h>
+
+int main ()
+{
+    using namespace interval_dict;
+    using namespace interval_dict::date_literals;
+    using namespace std::string_literals;
+
+    //! [Simple Example]
+    // Mix and match open and closed intervals
+    using DateDict = IntervalDictICL<std::string, int,
+                                  boost::gregorian::date>;
+
+    DateDict interval_dict(DateDict::KeyValueIntervals
+    {
+        {"aa"s, 0, {20200110_dt, 20200120_dt}},
+        {"aa"s, 1, {20200115_dt, 20200125_dt}},
+        {"bb"s, 2, {20200201_dt, 20200205_dt}},
+        // Specify an inclusive/closed interval
+        {"bb"s, 3, DateDict::IntervalType::closed(20200228_dt, 20200228_dt)},
+    });
+
+    std::cout << interval_dict << "\n";
+    //! [Simple Example]
+
+    /*
+       //! [Output]
+        aa	[0]	[2020-Jan-10,2020-Jan-15)
+        aa	[0, 1]	[2020-Jan-15,2020-Jan-20)
+        aa	[1]	[2020-Jan-20,2020-Jan-25)
+        bb	[2]	[2020-Feb-01,2020-Feb-05)
+        bb	[3]	[2020-Feb-28,2020-Feb-28]
+       //! [Output]
+     */
+
+
+    return 0;
+}
