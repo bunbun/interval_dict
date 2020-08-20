97a2e14431ec684af007365274372f55f264499f
Leo Goodstadt <github@llew.org.uk>
2020-08-20 17:45:52 +0100

    Add tests for 'inverse' members only in BiIntervalDict
    resolves #1
    
    These include:
      - count_value()
      - contains_value()
      - inverse_find()
      - values()
      - inverse_size()
      - subset_values()
    
    Theses tests mostly leverage those for the corresponding
    functions in the "forward" direction
    (`count()` for `count_value()`) often checking that inverting
    the dictionary and applying the inverse operation gives the
    same result. E.g.
    
      dict.invert().values() == dict.keys();

--- tests/test_inverse_find.cpp
+++ tests/test_inverse_find.cpp
@@ -1,4 +1,181 @@
+//  IntervalDict library
 //
-// Created by lg on 20/08/2020.
+//  Copyright Leo Goodstadt 2020-present
 //
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0.
+//  (See http://www.boost.org/LICENSE_1_0.txt)
+//
+//  Project home: https://github.com/goodstadt/intervaldict
+//
+/// \file test_icl_find.cpp
+/// \brief Test IntervalDict::find()
+/// \author Leo Goodstadt
+/// Contact intervaldict@llew.org.uk
 
+#include "catch.hpp"
+#include "test_data.h"
+#include "test_utils.h"
+
+#include <interval_dict/gregorian.h>
+#include <interval_dict/intervaldicticl.h>
+#include <interval_dict/ptime.h>
+#include <vector>
+
+TEMPLATE_TEST_CASE("Test inverse_find() for different interval types",
+                   "[find]",
+                   boost::icl::interval<int>::type,
+                   boost::icl::left_open_interval<int>,
+                   boost::icl::right_open_interval<int>,
+                   boost::icl::open_interval<int>,
+                   boost::icl::closed_interval<int>,
+                   boost::icl::interval<float>::type,
+                   boost::icl::left_open_interval<float>,
+                   boost::icl::right_open_interval<float>,
+                   boost::icl::interval<boost::posix_time::ptime>::type,
+                   boost::icl::left_open_interval<boost::posix_time::ptime>,
+                   boost::icl::right_open_interval<boost::posix_time::ptime>,
+                   boost::icl::open_interval<boost::posix_time::ptime>,
+                   boost::icl::closed_interval<boost::posix_time::ptime>,
+                   boost::icl::discrete_interval<boost::posix_time::ptime>,
+                   boost::icl::interval<boost::gregorian::date>::type,
+                   boost::icl::left_open_interval<boost::gregorian::date>,
+                   boost::icl::right_open_interval<boost::gregorian::date>,
+                   boost::icl::open_interval<boost::gregorian::date>,
+                   boost::icl::closed_interval<boost::gregorian::date>,
+                   boost::icl::discrete_interval<boost::gregorian::date>)
+{
+    using namespace std::string_literals;
+    using Interval = TestType;
+    using Key = std::string;
+    using Val = int;
+    using IDict = interval_dict::INTERVALDICTTESTTYPE<Key, Val, Interval>;
+    using Interval = typename IDict::IntervalType;
+
+    /*
+     * TestData
+     */
+    GIVEN("An IntervalDict with overlapping intervals")
+    {
+        TestData<Interval> test_data;
+        const IDict test_dict(test_data.initial());
+        const auto inverse_dict = test_dict.invert();
+
+
+        // N.B. "ee" should be ignored
+        const auto all_keys = std::vector{"aa"s, "bb"s, "cc"s, "dd"s, "ee"s};
+        const auto adjust = Adjust<Interval>{};
+        const auto expected = test_data.intervals();
+        const auto query = test_data.query_interval_for_find();
+        const auto queries = test_data.query_intervals_for_find();
+        const auto query_end =
+            Interval{boost::icl::upper(query), boost::icl::upper(query)};
+        const auto query_max =
+            Interval{interval_dict::IntervalTraits<Interval>::max(),
+                     interval_dict::IntervalTraits<Interval>::max()};
+
+        WHEN("Calling find() the largest extent")
+        {
+            const auto max_ext = interval_dict::interval_extent<Interval>;
+            THEN("Expect all corresponding values for those keys")
+            {
+                REQUIRE(inverse_dict.inverse_find(all_keys, max_ext) ==
+                        std::vector{0, 1, 2, 3, 5, 6, 7});
+                REQUIRE(inverse_dict.inverse_find({"bb"s, "dd"s}, max_ext) ==
+                        std::vector{1, 2, 5, 6, 7});
+                REQUIRE(inverse_dict.inverse_find("bb"s, max_ext) == std::vector{1, 2});
+                REQUIRE(inverse_dict.inverse_find("bb"s,
+                                       boost::icl::lower(max_ext),
+                                       boost::icl::upper(max_ext)) ==
+                        std::vector{1, 2});
+                REQUIRE(inverse_dict.inverse_find("not a key"s,
+                                       boost::icl::lower(max_ext),
+                                       boost::icl::upper(max_ext)) ==
+                        std::vector<Val>());
+            }
+        }
+
+        WHEN("Calling find() with a query extent")
+        {
+            THEN("Expect all corresponding values for those keys only over "
+                 "that interval")
+            {
+                REQUIRE(inverse_dict.inverse_find(all_keys, adjust.both(query)) ==
+                        std::vector{1, 2, 3, 5});
+                REQUIRE(inverse_dict.inverse_find({"bb"s, "dd"s}, query) ==
+                        std::vector{1, 2, 5});
+                REQUIRE(inverse_dict.inverse_find("bb"s, query) == std::vector{1, 2});
+                // nothing for this key
+                REQUIRE(inverse_dict.inverse_find("aa"s, adjust.both(query)) ==
+                        std::vector<Val>());
+                // not a key
+                REQUIRE(inverse_dict.inverse_find("not a key"s, query) ==
+                        std::vector<Val>());
+            }
+        }
+
+        WHEN("Calling find() with a single points")
+        {
+            THEN("Expect all corresponding values for those keys")
+            {
+                REQUIRE(inverse_dict.inverse_find("bb"s,
+                                       boost::icl::lower(query),
+                                       boost::icl::upper(query)) ==
+                        std::vector{1, 2});
+                // Inclusive single point
+                if (!boost::icl::is_empty(query_end))
+                {
+                    REQUIRE(inverse_dict.inverse_find(all_keys, query_end) ==
+                            std::vector{1, 2, 3, 5});
+                    REQUIRE(
+                        inverse_dict.inverse_find("bb"s, boost::icl::lower(query_end)) ==
+                        std::vector{1, 2});
+                }
+            }
+        }
+
+        WHEN("Calling find() with a multiple intervals")
+        {
+            THEN("Expect all corresponding values for those keys")
+            {
+                REQUIRE(inverse_dict.inverse_find("aa"s, queries) == std::vector{0});
+                REQUIRE(inverse_dict.inverse_find("bb"s, queries) == std::vector{1});
+                REQUIRE(inverse_dict.inverse_find("dd"s, queries) == std::vector{6, 7});
+                // not a key
+                REQUIRE(inverse_dict.inverse_find("not a key"s, queries) ==
+                        std::vector<Val>());
+                // Empty intervals
+                REQUIRE(inverse_dict.inverse_find("dd"s,
+                                       interval_dict::Intervals<Interval>()) ==
+                        std::vector<Val>{});
+                REQUIRE(inverse_dict.inverse_find("not a key"s,
+                                       interval_dict::Intervals<Interval>()) ==
+                        std::vector<Val>{});
+            }
+        }
+
+        WHEN("Calling find() with an empty query extent")
+        {
+            THEN("Expect all empty results")
+            {
+                // Empty query interval:
+                if (boost::icl::is_empty(query_end))
+                {
+                    REQUIRE(inverse_dict.inverse_find(all_keys, query_end) ==
+                            std::vector<Val>{});
+                }
+                REQUIRE(inverse_dict.inverse_find(all_keys, query_max) ==
+                        std::vector<Val>{});
+                REQUIRE(inverse_dict.inverse_find("bb"s, query_max) == std::vector<Val>{});
+
+                // Empty query interval: Empty result
+                REQUIRE(inverse_dict.inverse_find("bb"s,
+                                       boost::icl::upper(query_max),
+                                       boost::icl::upper(query_max)) ==
+                        std::vector<Val>{});
+                REQUIRE(inverse_dict.inverse_find("bb"s, boost::icl::upper(query_max)) ==
+                        std::vector<Val>{});
+            }
+        }
+    }
+}
--- tests/test_inverse_member_functions.cpp
+++ tests/test_inverse_member_functions.cpp
@@ -1,4 +1,136 @@
 //
-// Created by lg on 20/08/2020.
+// Created by lg on 10/07/2020.
 //
+//  IntervalDict library
+//
+//  Copyright Leo Goodstadt 2020-present
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0.
+//  (See http://www.boost.org/LICENSE_1_0.txt)
+//
+//  Project home: https://github.com/goodstadt/intervaldict
+//
+/// \file test_icl_member_functions.cpp
+/// \brief Test IntervalDict memeber functions
+/// \author Leo Goodstadt
+/// Contact intervaldict@llew.org.uk
 
+#include "catch.hpp"
+#include "test_data.h"
+#include "test_utils.h"
+#include <interval_dict/gregorian.h>
+#include <interval_dict/intervaldicticl.h>
+#include <interval_dict/ptime.h>
+#include <vector>
+
+TEMPLATE_TEST_CASE("Test inverse member functions different interval types",
+                   "[keys][count][contains][size][clear][empty][invert]"
+                   "[values][count_value][contains_value][inverse_size]",
+                   boost::icl::interval<int>::type,
+                   boost::icl::left_open_interval<int>,
+                   boost::icl::right_open_interval<int>,
+                   boost::icl::closed_interval<int>,
+                   boost::icl::open_interval<int>,
+                   boost::icl::interval<float>::type,
+                   boost::icl::left_open_interval<float>,
+                   boost::icl::right_open_interval<float>,
+                   boost::icl::interval<boost::posix_time::ptime>::type,
+                   boost::icl::left_open_interval<boost::posix_time::ptime>,
+                   boost::icl::right_open_interval<boost::posix_time::ptime>,
+                   boost::icl::open_interval<boost::posix_time::ptime>,
+                   boost::icl::closed_interval<boost::posix_time::ptime>,
+                   boost::icl::discrete_interval<boost::posix_time::ptime>,
+                   boost::icl::interval<boost::gregorian::date>::type,
+                   boost::icl::left_open_interval<boost::gregorian::date>,
+                   boost::icl::right_open_interval<boost::gregorian::date>,
+                   boost::icl::open_interval<boost::gregorian::date>,
+                   boost::icl::closed_interval<boost::gregorian::date>,
+                   boost::icl::discrete_interval<boost::gregorian::date>)
+{
+    using namespace std::string_literals;
+    using namespace interval_dict::date_literals;
+    using namespace boost::gregorian;
+    using Interval = TestType;
+    using Key = std::string;
+    using Val = int;
+    using IDict = interval_dict::INTERVALDICTTESTTYPE<Key, Val, Interval>;
+    using Interval = typename IDict::IntervalType;
+    TestData<Interval> test_data;
+    auto import_data = test_data.intervals();
+
+    /*
+     * TestData
+     */
+    GIVEN("An IntervalDict divided into subsets by keys")
+    {
+        // keys
+        const IDict test_dict(test_data.initial());
+        const auto inverse_dict = test_dict.invert();
+        const auto all_keys = test_dict.keys();
+
+        WHEN("we look at the keys of each subset")
+        {
+            THEN("They need to have the keys they were subset() with")
+            {
+                for (const auto& key : all_keys)
+                {
+                    const auto subset_dict = inverse_dict.subset_values(std::vector{key}).invert();
+                    REQUIRE(test_dict.subset(std::vector{key}) ==
+                            subset_dict);
+                    // only has one key as keys()
+                    REQUIRE(subset_dict.keys() == std::vector{key});
+                    REQUIRE(subset_dict.invert().values() == std::vector{key});
+
+                    // check not empty but has size == 1
+                    REQUIRE(!subset_dict.is_empty());
+                    REQUIRE(subset_dict.size() == 1);
+                    REQUIRE(subset_dict.invert().inverse_size() == 1);
+
+                    // but is empty after removing that key
+                    REQUIRE(copy(subset_dict).erase(key).is_empty());
+
+                    for (const auto& check_key : all_keys)
+                    {
+                        // count () == 1 and contains() == true only for the
+                        // matching key
+                        REQUIRE(subset_dict.count(check_key) ==
+                                (key == check_key ? 1 : 0));
+                        REQUIRE(subset_dict.invert().count_value(check_key) ==
+                                (key == check_key ? 1 : 0));
+                        REQUIRE(subset_dict.invert().contains_value(check_key) ==
+                                (key == check_key));
+                    }
+                }
+            }
+        }
+
+        GIVEN("An inverted IntervalDict divided into subsets by inverted keys")
+        {
+            // keys
+            const auto inverted_dict = test_dict.invert();
+            const auto all_values = inverted_dict.keys();
+            REQUIRE(all_values == test_dict.values());
+            WHEN("Examining each subset")
+            {
+                THEN("They need to match the subset divided by values")
+                {
+                    for (const auto& value : all_values)
+                    {
+                        const auto value_subset_dict1 =
+                            inverted_dict.subset(std::vector{value}).invert();
+                        const auto value_subset_dict2 =
+                            test_dict.subset(all_keys, std::vector{value});
+                        REQUIRE(value_subset_dict1 == value_subset_dict2);
+
+                        const auto value_subset_dict3 =
+                            test_dict.subset_values(std::vector{value});
+                        const auto value_subset_dict4 =
+                            test_dict.subset(all_keys, std::vector{value});
+                        REQUIRE(value_subset_dict3 == value_subset_dict4);
+                    }
+                }
+            }
+        }
+    }
+}
